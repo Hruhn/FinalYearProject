63a03e91b10221afbffea4d521d78154c6b3173c
Hruhn <98754029+Hruhn@users.noreply.github.com>
2023-11-14 21:33:39 +0800

    Update Serial.cpp

--- Serial.cpp
+++ Serial.cpp
@@ -55,3 +55,45 @@ int main() {
     close(serialPort);
     return 0;
 }
+
+/*
+// 修改解析串口数据的函数，添加条件判断
+void parseSensorData(const char *data, struct SensorData *sensorData) {
+    // 添加判断条件，确保数据包含有效的传感器信息
+    if (strstr(data, "id:") == NULL || strstr(data, "rtt_est:") == NULL || strstr(data, "rtt_raw:") == NULL ||
+        strstr(data, "dist_est:") == NULL || strstr(data, "average rssi:") == NULL || strstr(data, "num_frames:") == NULL) {
+        // 数据格式不符合预期，不执行解析
+        printf("Invalid data format: %s\n", data);
+        return;
+    }
+
+    char tempData[strlen(data) + 1];
+    strcpy(tempData, data);
+
+    char *token = strtok(tempData, ",");
+    while (token != NULL) {
+        char key[20];
+        int value;
+
+        // 添加条件判断，确保解析的键值对格式正确
+        if (sscanf(token, "%19[^:]:%d", key, &value) == 2) {
+            if (strcmp(key, "id") == 0) {
+                strncpy(sensorData->id, token + 3, sizeof(sensorData->id) - 1);
+                sensorData->id[sizeof(sensorData->id) - 1] = '\0';
+            } else if (strcmp(key, "rtt_est") == 0) {
+                sensorData->rtt_est = value;
+            } else if (strcmp(key, "rtt_raw") == 0) {
+                sensorData->rtt_raw = value;
+            } else if (strcmp(key, "dist_est") == 0) {
+                sensorData->dist_est = value;
+            } else if (strcmp(key, "average rssi") == 0) {
+                sensorData->average_rssi = value;
+            } else if (strcmp(key, "num_frames") == 0) {
+                sensorData->num_frames = value;
+            }
+        }
+
+        token = strtok(NULL, ",");
+    }
+}
+*/
